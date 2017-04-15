93bd6104bc68d7181473652725cb57a4a6464c7f
Moshe Zadka <zadka.moshe@gmail.com>
2017-04-14 18:53:55 -0700

    tests

---
+++ src/caparg/tests/test_commands.py
@@ -0,0 +1,39 @@
+import unittest
+
+import caparg
+
+NICE_COMMANDS = caparg.Collector()
+
+EVIL_COMMANDS = caparg.Collector()
+
+@NICE_COMMANDS.register()
+def main1(args):
+    return 'main1', args
+
+@NICE_COMMANDS.register(name='foo')
+def main2(args):
+    return 'main2', args
+
+@NICE_COMMANDS.register(name='bar')
+@EVIL_COMMANDS.register(name='foo')
+def main3(args):
+    return 'main3', args
+
+@EVIL_COMMANDS.register(name='baz')
+def main4(args):
+    return 'main4', args
+
+class CapargTest(unittest.TestCase):
+
+    def test_collecting(self):
+        collected = NICE_COMMANDS.collect()
+        self.assertIn('main1', collected)
+        self.assertIs(collected['main1'], main1)
+        self.assertNotIn('baz', collected)
+
+    def test_non_collision(self):
+        nice = NICE_COMMANDS.collect()
+        evil = EVIL_COMMANDS.collect()
+        self.assertIs(nice['foo'], main2)
+        self.assertIs(nice['bar'], main3)
+        self.assertIs(evil['foo'], main3)
