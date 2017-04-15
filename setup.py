1c0182bd3dce8530963598353b6372633022d1a9
Moshe Zadka <zadka.moshe@gmail.com>
2017-04-14 18:39:19 -0700

    Skeleton

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name='caparg',
+    license='MIT',
+    description="Captain Arguments.",
+    long_description="Such Captain. So Argument.",
+    use_incremental=True,
+    setup_requires=['incremental'],
+    author="Moshe Zadka",
+    author_email="zadka.moshe@gmail.com",
+    packages=setuptools.find_packages(where='src'),
+    package_dir={"": "src"},
+    install_requires=['incremental', 'venusian'],
+    entry_points={
+        'caparg': [
+             "caparg=caparg:caparg",
+        ]
+    }
+)
