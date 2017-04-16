e01fbd21209640104bb6dd65d60b1515cf4042e5
Moshe Zadka <zadka.moshe@gmail.com>
2017-04-16 12:21:41 -0700

    working selftest

--- src/caparg/example/selftest.py
+++ src/caparg/example/selftest.py
@@ -1,7 +1,19 @@
 from __future__ import print_function
 
+import ast
+import subprocess
+import sys
+
 from caparg.example import main as cemain
 
 @cemain.COMMANDS.register()
 def selftest(args):
-    raise SystemExit("not working")
+    command_prefix = [sys.executable, '-m', 'caparg.example']
+    hello = command_prefix + ['hello', 'world']
+    res = subprocess.check_output(hello).decode('utf-8')
+    greeting, contents = res.split(None, 1)
+    if greeting != 'Hello':
+        raise ValueError("Incorrect greeting", res)
+    parsed = ast.literal_eval(contents)
+    if parsed != ['hello', 'world']:
+        raise ValueError("Incorrect arguments", res)
