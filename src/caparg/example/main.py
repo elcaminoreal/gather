34d18fc5a79734f2ba1135a7683966efcbfe6238
Moshe Zadka <zadka.moshe@gmail.com>
2017-04-16 11:12:35 -0700

    Add example of collecting things that are not commands

--- src/caparg/example/main.py
+++ src/caparg/example/main.py
@@ -4,6 +4,8 @@ import caparg
 
 COMMANDS = caparg.Collector()
 
+BREAKFAST = caparg.Collector()
+
 @COMMANDS.register()
 def hello(args):
     print("Hello", args)
@@ -11,3 +13,32 @@ def hello(args):
 @COMMANDS.register()
 def goodbye(args):
     print("Goodbye", args)
+
+@COMMANDS.register()
+def breakfast(args):
+    foods = [klass() for klass in BREAKFAST.collect().values()]
+    for food in foods:
+        food.prepare()
+    for food in foods:
+        food.eat()
+
+@BREAKFAST.register()
+class Eggs(object):
+    def prepare(self):
+        print("Scrambling eggs")
+    def eat(self):
+        print("Devouring eggs")
+
+@BREAKFAST.register()
+class Cereal(object):
+    def prepare(self):
+        print("Mixing cereal and milk")
+    def eat(self):
+        print("Eating cereal with a spoon")
+
+@BREAKFAST.register()
+class OrangeJuice(object):
+    def prepare(self):
+        print("Squeezing orange juice")
+    def eat(self):
+        print("Drinking orange juice")
