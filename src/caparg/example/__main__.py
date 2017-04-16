26b5430b0980de92df8b429f99f379fb36d9b511
Moshe Zadka <zadka.moshe@gmail.com>
2017-04-16 12:40:34 -0700

    improve stuff

--- src/caparg/example/__main__.py
+++ src/caparg/example/__main__.py
@@ -8,8 +8,8 @@ if __name__ != '__main__':
     raise ImportError("module cannot be imported")
 
 caparg.run(
-   argv=sys.argv[1:],
-   commands=main.COMMANDS.collect(),
-   version=caparg.__version__,
-   output=sys.stdout,
+    argv=sys.argv[1:],
+    commands=main.COMMANDS.collect(),
+    version=caparg.__version__,
+    output=sys.stdout,
 )
